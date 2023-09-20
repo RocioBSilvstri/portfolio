b25c6f6b0b0eb5e59353c5521125f2e72bc5e73a
RocioBSilvstri <rociobsilvestri@gmail.com>
2023-09-20 13:47:14 -0300

    changes200923

---
+++ JavaScript/java.js
@@ -0,0 +1,23 @@
+let firstname = "Rocio"
+let lastname = "Silvestri"
+let empty = "" // string
+let age = "31" //number 
+let willDinner = undefined //undefined
+let test // undefined 
+let hasPets = true //boolean 
+let knowsHowToDrive = false //boolean 
+let nulo //null
+let notANumber = NaN //NaN
+
+//console.Log (firstname)
+// document.erite (firstname)
+
+//concatenar
+
+console.log(firstname + " " + lastname)
+
+// literal strings - template literals 
+console.log (`${firstname} ${lastname}`)
+
+//typeof -sirve para preguntar que tipo de dato es una variable y me retorna un tring
+console.log(typeof firstname)
